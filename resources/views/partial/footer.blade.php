ffdf7172facfc811e31c95bd7fed9f9468eccfc5
saarahchairi <sarni555@hotmail.com>
2022-08-09 16:26:33 +0200

    footer done

---
+++ resources/views/partial/footer.blade.php
@@ -0,0 +1,3 @@
+<section class="items-center text-center px-6 py-4 bg-gray-800 dark:bg-gray-800">
+    <p class="py-2 text-white dark:text-gray-800 sm:py-0">Copyright © Your Exercice 2020</p>
+</section>
